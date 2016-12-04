eeb730393f46a41620c2cf80bd8b680d72b80710
Janne Blackniemi <jannemustaniemi@hotmail.com>
2016-12-05 01:02:16 +0200

    Rotated rectangle calculation and its center pixel colors

--- app/src/main/java/sop/augmentedandroids/MainActivity.java
+++ app/src/main/java/sop/augmentedandroids/MainActivity.java
@@ -12,6 +12,7 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
@@ -24,6 +25,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +41,9 @@ public class MainActivity extends Activity implements CvCameraViewListener, View
     Camera c = Camera.open();
 
     static Rect refRect = new Rect(0,0,0,0);
+    static RotatedRect rotRect = new RotatedRect();
+
+    Scalar textCol = new Scalar(255, 0, 255);
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 
@@ -114,6 +120,8 @@ public class MainActivity extends Activity implements CvCameraViewListener, View
 
     public Mat onCameraFrame(Mat inputFrame) {
 
+
+        // Skip calculating contour for as many frames as indicated by 'frameskip' variable
         if(frame_i < frameskip) {
             frame_i++;
         } else {
@@ -122,10 +130,60 @@ public class MainActivity extends Activity implements CvCameraViewListener, View
         }
 
         //UpdateContours(inputFrame);
-        Core.rectangle(inputFrame, refRect.tl(), refRect.br(), new Scalar(0,255,10,255), 3);
-        return inputFrame;
 
-        //Core.putText(frame_out, String.valueOf(biggestContourArea), new Point(50.0, 140.0), Core.FONT_HERSHEY_PLAIN, 7, new Scalar(200,150,250), 3);
+        // DRAW THE BOUNDING RECTANGLE (GREEN ONE)
+        Core.rectangle(inputFrame, refRect.tl(), refRect.br(), new Scalar(0,255,10,255), 2);
+
+
+        // CALCULATE ROTATED RECTANGLE (MAGENTA ONE)
+        Point[] ps = new Point[4];
+        rotRect.points(ps);
+        List<MatOfPoint> rotRectCnt = new ArrayList<>();
+        rotRectCnt.add(new MatOfPoint(ps));
+
+        Imgproc.drawContours(inputFrame, rotRectCnt, -1, new Scalar(255, 0, 255), 2);   // Draw rotated rect into image frame
+
+
+        // CALCULATE AVG RECT SIDE LENGTH
+        NumberFormat nF = new DecimalFormat("#0.00");
+        Core.putText(inputFrame, "avg side length: " + nF.format(RectAvgLength(ps)), new Point(10.0, 100), Core.FONT_HERSHEY_PLAIN, 3, textCol, 3);
+
+
+        // CENTER OF ROTATED RECT
+        Point rectCenterPoint = rotRect.center;
+
+        double[] rectCenterColDoubles = {0.0, 0.0, 0.0};
+        rectCenterColDoubles = inputFrame.get(DoubleToInt(rectCenterPoint.y), DoubleToInt(rectCenterPoint.x));
+        // Workaround for app crash in a case where couldn't acquire color values of rect center
+        if(rectCenterColDoubles != null) {
+            Scalar rectCenterCols = new Scalar(DoubleToInt(rectCenterColDoubles[0]), DoubleToInt(rectCenterColDoubles[1]), DoubleToInt(rectCenterColDoubles[2]));
+
+            Core.putText(inputFrame, "R: " + String.valueOf(rectCenterColDoubles[0]), new Point(900.0, 40), Core.FONT_HERSHEY_PLAIN, 3, textCol, 3);
+            Core.putText(inputFrame, "G: " + String.valueOf(rectCenterColDoubles[1]), new Point(900.0, 100), Core.FONT_HERSHEY_PLAIN, 3, textCol, 3);
+            Core.putText(inputFrame, "B: " + String.valueOf(rectCenterColDoubles[2]), new Point(900.0, 160), Core.FONT_HERSHEY_PLAIN, 3, textCol, 3);
+
+            Core.circle(inputFrame, rectCenterPoint, 10, textCol, 3);
+        }
+
+
+        return inputFrame;  // Return the final edited image frame
+    }
+
+    int DoubleToInt(double a) {
+        return Integer.valueOf((int) Math.round(a));
+    }
+
+    double RectAvgLength(Point[] ps) {
+        double L1 = GetDist(ps[0], ps[1]);
+        double L2 = GetDist(ps[1], ps[2]);
+        double L3 = GetDist(ps[2], ps[3]);
+        double L4 = GetDist(ps[3], ps[0]);
+
+        return ((L1 + L2 + L3 + L4)/4);
+    }
+
+    double GetDist(Point p1, Point p2) {
+        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
     }
 
     public synchronized void UpdateContours(Mat frame_in) {
@@ -140,6 +198,7 @@ public class MainActivity extends Activity implements CvCameraViewListener, View
 
         Imgproc.Canny(frame, frame, 50.0, 175.0);
         Imgproc.dilate(frame, frame, new Mat(), new Point(-1,-1), 1);   // Improves ignoring of small shapes that are not squarish, fps impact of 1
+        //Imgproc.erode(frame, frame, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
 
         Imgproc.findContours(frame, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
@@ -173,6 +232,10 @@ public class MainActivity extends Activity implements CvCameraViewListener, View
         MatOfPoint biggestContour = new MatOfPoint(curve.toArray());
 
         refRect = Imgproc.boundingRect(biggestContour);
+
+        //MatOfPoint2f points = new MatOfPoint2f()
+
+        rotRect = Imgproc.minAreaRect(curve);
 
         //Log.d(TAG, Double.toString(biggestContourArea));
     }
